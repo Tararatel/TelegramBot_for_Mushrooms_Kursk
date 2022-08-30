3a3faa3575ffa079d24e3946ed945f5607c263ba
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-08-30 18:11:58 +0300

    bd connect

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const TelegramApi = require('node-telegram-bot-api');
+const sequelize = require('./db');
+const UserModel = require('./models');
 
 const token = '5371501689:AAH814sNx68iyVyjROFSiDZM6WalKoRBzck';
 
 const bot = new TelegramApi(token, { polling: true });
 
-const start = () => {
+const start = async () => {
+	try {
+		await sequelize.authenticate();
+		await sequelize.sync();
+	} catch (e) {
+		console.log('Подключение к БД не удалось', e);
+	}
+
 	bot.setMyCommands([
 		{ command: '/start', description: 'Запустить бота' },
 		{ command: '/send', description: 'Отправить координаты' },
 	]);
 
-	bot.on('message', async (msg) => {
-		const text = msg.text;
-		const chatId = msg.chat.id;
+	try {
+		bot.on('message', async (msg) => {
+			const text = msg.text;
+			const chatId = msg.chat.id;
 
-		if (text === '/start') {
-			await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/81a/4f6/81a4f635-870c-370c-830d-02004b54e0a8/6.webp');
-			await bot.sendMessage(chatId, `Приветствую тебя, путник!`);
-		}
-		return bot.sendMessage(chatId, 'Эта команда не поддерживается ботом');
-	});
+			if (text === '/start') {
+				await bot.sendSticker(
+					chatId,
+					'https://tlgrm.ru/_/stickers/81a/4f6/81a4f635-870c-370c-830d-02004b54e0a8/6.webp'
+				);
+				await bot.sendMessage(chatId, `Приветствую тебя, путник!`);
+			}
+			return bot.sendMessage(chatId, 'Эта команда не поддерживается ботом');
+		});
+	} catch (e) {
+		console.log('Какая-то ошибка', e);
+	}
 };
 
 start();
---
+++ models.js
@@ -0,0 +1,8 @@
+const sequelize = require('./db');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('user', {
+	id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
+	chatId: { type: DataTypes.STRING, unique: true },
+	coordinates: { type: DataTypes.STRING, unique: true },
+});
