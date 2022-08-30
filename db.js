b6cf7611285b9b13297a894685dab159e6674a89
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-08-30 19:06:13 +0300

    fix code

--- db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
-
-module.exports = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-	host: DB_HOST,
-	dialect: 'postgres',
-	logging: false,
-});
--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const TelegramApi = require('node-telegram-bot-api');
 const sequelize = require('./db');
 const UserModel = require('./models');
 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+	'dbud7mam0fhl9k',
+	'yleqgcldfqhvkh',
+	'e41c35835b8a139606e4ff4b79bf52ea5533d40ec9ecf64151aa739ae8f2d623',
+	{
+		host: 'ec2-54-246-185-161.eu-west-1.compute.amazonaws.com',
+		dialect: 'postgres',
+	}
+);
+
 const token = '5371501689:AAH814sNx68iyVyjROFSiDZM6WalKoRBzck';
 
 const bot = new TelegramApi(token, { polling: true });
@@ -9,6 +21,7 @@ const bot = new TelegramApi(token, { polling: true });
 const start = async () => {
 	try {
 		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
 		await sequelize.sync();
 	} catch (e) {
 		console.log('Подключение к БД не удалось', e);
